0dbf272d85205ca3ad59a4e94cc591fef55b61f6
SameeraRukshan <watchsameera@gmail.com>
2026-02-13 17:47:00 +0530

    Add iOS Flutter platform scaffold and build docs

---
+++ lib/data/category_promo_images.dart
@@ -0,0 +1,15 @@
+const Map<String, String> categoryPromoImages = <String, String>{
+  'Restaurant':
+      'https://drive.google.com/uc?export=view&id=1CMZzEObCHPTl6vFZBcdM1BI_CVqq7K9t',
+  'Salon':
+      'https://drive.google.com/uc?export=view&id=1CMZzEObCHPTl6vFZBcdM1BI_CVqq7K9t',
+  'Gym':
+      'https://drive.google.com/uc?export=view&id=1CMZzEObCHPTl6vFZBcdM1BI_CVqq7K9t',
+  'Clinic':
+      'https://drive.google.com/uc?export=view&id=1CMZzEObCHPTl6vFZBcdM1BI_CVqq7K9t',
+  'Retail':
+      'https://drive.google.com/uc?export=view&id=1CMZzEObCHPTl6vFZBcdM1BI_CVqq7K9t',
+};
+
+String fallbackPromoImage =
+    'https://drive.google.com/drive/folders/1CMZzEObCHPTl6vFZBcdM1BI_CVqq7K9t';
